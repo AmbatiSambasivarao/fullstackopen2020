4717cca27ad8852da614073bb4625f4d0563e2fd
AmbatiSambasivarao <85328990+AmbatiSambasivarao@users.noreply.github.com>
2021-06-16 10:04:07 +0530

    Add files via upload

---
+++ part 8/graphql-phonebook-frontend-part8-6/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import { useQuery, useApolloClient } from '@apollo/client'
+
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
+
+import { ALL_PERSONS } from './queries'
+
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
+  )
+}
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <button onClick={logout}>
+        logout
+      </button>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons = {result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </div>
+  )
+}
+
+export default App
---
+++ part 8/graphql-phonebook-frontend-part8-9/src/App.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import { useQuery,useMutation, useApolloClient, useSubscription } from '@apollo/client'
+
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
+
+import { ALL_PERSONS, PERSON_ADDED } from './queries'
+
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
+  )
+}
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  const updateCacheWith = (addedPerson) => {
+    const includedIn = (set, object) => 
+      set.map(p => p.id).includes(object.id)  
+
+    const dataInStore = client.readQuery({ query: ALL_PERSONS })
+    if (!includedIn(dataInStore.allPersons, addedPerson)) {
+      client.writeQuery({
+        query: ALL_PERSONS,
+        data: { allPersons : dataInStore.allPersons.concat(addedPerson) }
+      })
+    }   
+  }
+
+  useSubscription(PERSON_ADDED, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      const addedPerson = subscriptionData.data.personAdded
+      notify(`${addedPerson.name} added`)
+      updateCacheWith(addedPerson)
+      console.log(subscriptionData)
+    }
+  })
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <button onClick={logout}>
+        logout
+      </button>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons = {result.data.allPersons} />
+      <PersonForm setError={notify} updateCacheWith={updateCacheWith} />
+      <PhoneForm setError={notify} />
+    </div>
+  )
+}
+
+export default App
